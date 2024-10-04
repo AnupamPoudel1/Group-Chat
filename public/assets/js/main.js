5e751d15fe3e5aa355cb4b0e1be0c6bdc2a8a885
Anupam <anupampoudel78@gmail.com>
2024-10-04 23:16:37 +0545

    Feature: Added total clients count and feature to log messages in console

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,6 +2,14 @@ const socket = io();
 
 const form = document.getElementById('message-form');
 const input = document.getElementById('message-input');
+const name = document.getElementById('name-input');
+const messageContaoner = document.getElementById('message-container');
+const clients = document.getElementById('totalClients');
+
+socket.on('totalClients', (data) => {
+    console.log(data);
+    clients.innerText = `Total clients: ${data}`;
+});
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
--- server.js
+++ server.js
@@ -17,15 +17,39 @@ const server = createServer(app);
 // setting up socket io
 const io = new Server(server);
 
-io.on('connection', (socket) => {
+// function to count number of users connected in the socket
+
+let connectedSockets = new Set();
+
+const socketConnected = (socket) => {
     console.log(socket.id + ' user connected');
+    connectedSockets.add(socket.id);
+
+    io.emit('totalClients', connectedSockets.size);
+
     socket.on('chat message', (msg) => {
         console.log(socket.id + " messaged: " + msg);
-    })
+        io.emit('chat message', msg);
+    });
     socket.on('disconnect', () => {
         console.log(socket.id + ' user disconnected');
-    })
-})
+        connectedSockets.delete(socket.id);
+        io.emit('totalClients', connectedSockets.size);
+    });
+}
+
+io.on('connection', socketConnected);
+
+// io.on('connection', (socket) => {
+//     console.log(socket.id + ' user connected');
+//     socket.on('chat message', (msg) => {
+//         console.log(socket.id + " messaged: " + msg);
+//         io.emit('chat message', msg);
+//     });
+//     socket.on('disconnect', () => {
+//         console.log(socket.id + ' user disconnected');
+//     });
+// });
 
 // url encoded
 app.use(express.urlencoded({ extended: false }));
